f41682cc057004158a6fb7f3ebafbde66268a077
Emily Chang <Emily@Emilys-MacBook-Pro.local>
2017-01-08 20:43:58 -0800

    new form

--- main.js
+++ main.js
@@ -29,6 +29,16 @@ function showContact() {
 function showForm() {
     $(".slide").fadeOut("slow").animate({opacity : '0'}, 100);
     $("#apply").fadeIn("slow").animate({opacity : '1'}, 300).css("z-index", 199);
+    if(!window.ConversationalForm){
+        window.ConversationalForm = new cf.ConversationalForm({
+            formEl: document.getElementById("conversational"),
+            context: document.getElementById("apply-form"),
+            submitCallback: function() {
+                $(".submit").click()
+            },
+            userImage: "none"
+        });
+    }
 };
 
 function thankYou() {
